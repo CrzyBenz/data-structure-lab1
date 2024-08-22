a2d6d8262a7303bb286b27ddf74c39903c5a4a9e
Benz <ehillabenz20@gmail.com>
2024-08-22 12:01:46 +0800

    initial commit

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
-num_1 = input("Enter first number: ")
-num_2 = input("Enter Second number: ")
-
-if num_1 < num_2:
-    print(num_1 + " is less than " + num_2)
-
-elif num_1 > num_2:
-    print(num_1 + " is greater than " + num_2)
-
-else:
-     print(num_1 + " is equal to " + num_2)
-
+# num_1 = int(input("Enter first number: "))
+# num_2 = int(input("Enter Second number: "))
+#
+# if num_1 < num_2:
+#     print(f"{num_1} is less than {num_2}")
+#
+# elif num_1 > num_2:
+#     print(f"{num_1} is greater than {num_2}")
+#
+# else:
+#      print(f"{num_1} is equal to {num_2}")
+#
 #
 #
 # name_1 = input("Enter classmate name 1 : ")
