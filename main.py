117301db82190ace65bdeb3e4c0afc33df9a1219
Benz <ehillabenz20@gmail.com>
2024-08-22 11:48:03 +0800

    initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+num_1 = input("Enter first number: ")
+num_2 = input("Enter Second number: ")
+
+if num_1 < num_2:
+    print(num_1 + " is less than " + num_2)
+
+elif num_1 > num_2:
+    print(num_1 + " is greater than " + num_2)
+
+else:
+     print(num_1 + " is equal to " + num_2)
+
+#
+#
+# name_1 = input("Enter classmate name 1 : ")
+# name_2 = input("Enter classmate name 1 : ")
+# name_3 = input("Enter classmate name 1 : ")
+#
+# print ("Your classmates are: " + name_1 + ", " + name_2 + " & " + name_3)
+
